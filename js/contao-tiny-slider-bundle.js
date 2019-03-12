d4e4d8f43d638ce38cd6f61c59def9248c4d1c27
Dennis Patzer <d.patzer@heimrich-hannot.de>
2019-03-12 10:34:45 +0100

    initial commit

---
+++ js/contao-tiny-slider-bundle.js
@@ -0,0 +1,50 @@
+import {tns} from 'tiny-slider';
+
+class TinySliderBundle {
+    constructor(selector, forceInit) {
+        document.querySelectorAll(selector).forEach(element => {
+            TinySliderBundle.init(element, forceInit);
+        });
+    }
+
+    static init(element, forceInit) {
+        let container = element.querySelector('.tiny-slider-container'),
+            config = JSON.parse(container.getAttribute('data-tiny-slider-config'));
+
+        if (config.skipInit && (typeof forceInit === 'undefined' || !forceInit))
+        {
+            return;
+        }
+
+        config.container = element.querySelector(config.container);
+
+        let onInit = config.onInit;
+
+        config.onInit = function (e) {
+            element.classList.add('tiny-slider-initialized');
+
+            if(onInit){
+                let onInitFunction = new Function(onInit + '()');
+                onInitFunction();
+            }
+        };
+
+        let slider = tns(config);
+
+        TinySliderBundle.sliders.push(slider);
+    }
+
+    static getSliders() {
+        return TinySliderBundle.sliders;
+    }
+}
+
+TinySliderBundle.sliders = [];
+
+document.addEventListener('DOMContentLoaded', function () {
+    new TinySliderBundle('.tiny-slider');
+});
+
+window.TinySliderBundle = TinySliderBundle;
+
+export default TinySliderBundle;
